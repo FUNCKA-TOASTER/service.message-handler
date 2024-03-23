e58db88052f0f0144379cb7be16f1dda8ff4cb87
Oidaho <76991612+Oidaho@users.noreply.github.com>
2024-03-23 18:17:26 +1000

    Entrypoiunt

---
+++ start.py
@@ -0,0 +1,30 @@
+"""Service "toaster.comman-handling-service".
+About:
+    ...
+    
+Author:
+    Oidaho (Ruslan Bashinskii)
+    oidahomain@gmail.com
+"""
+import asyncio
+from consumer import consumer
+from handler import button_handler
+from logger import logger
+
+
+async def main():
+    """Entry point.
+    """
+    log_text = "Awaiting button events..."
+    await logger.info(log_text)
+
+    for data in consumer.listen_queue("messages"):
+        log_text = f"Recived new event: {data}"
+        await logger.info(log_text)
+
+        await button_handler(data)
+
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
