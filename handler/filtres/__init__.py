56c784c49ed4f772b510135c64d6caf551a80dee
Oidaho <76991612+Oidaho@users.noreply.github.com>
2024-04-05 12:50:30 +1000

    Filter list exetended

--- handler/filtres/__init__.py
+++ handler/filtres/__init__.py
@@ -1,10 +1,12 @@
 from .filtres import (
     SlowModeQueueFilter,
     ContentFilter,
+    OpenPMFilter,
 )
 
 # Filter in order of priority of execution
 filter_list = [
+    OpenPMFilter,
     ContentFilter,
     SlowModeQueueFilter,
 ]
