96c593fbd19d3d052700e529394ce43ccb0dda8c
Oidaho <76991612+Oidaho@users.noreply.github.com>
2024-04-16 21:41:33 +1000

    Filter list extended

--- handler/filtres/__init__.py
+++ handler/filtres/__init__.py
@@ -1,10 +1,19 @@
-from .filtres import SlowModeQueueFilter, ContentFilter, OpenPMFilter, AccountAgeFilter
+from .filtres import (
+    SlowModeQueueFilter,
+    ContentFilter,
+    OpenPMFilter,
+    AccountAgeFilter,
+    URLFilter,
+    CurseWordsFilter,
+)
 
 # Filter in order of priority of execution
 filter_list = [
     AccountAgeFilter,
     OpenPMFilter,
     ContentFilter,
+    URLFilter,
+    CurseWordsFilter,
     SlowModeQueueFilter,
 ]
 
