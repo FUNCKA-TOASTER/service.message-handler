59ce332c390d2867ad6b58fe878fd9e30f536277
Oidaho <76991612+Oidaho@users.noreply.github.com>
2024-07-30 18:02:18 +1000

    test commit -m

--- filters/filters.py
+++ filters/filters.py
@@ -13,14 +13,10 @@ class SlowModeQueue(BaseFilter):
     NAME = "Slow Mode"
 
     def _handle(self, event: Event) -> bool:
-        from loguru import logger
-
         setting = "slow_mode"
         status = self._is_setting_enabled(event, setting)
         if status == SettingStatus.inactive:
             return False
-
-        logger.debug(1)
 
         expired = get_user_queue_status(
             db_instance=TOASTER_DB,
@@ -40,15 +36,11 @@ class SlowModeQueue(BaseFilter):
 
             return True
 
-        logger.debug(2)
-
         insert_user_to_queue(
             db_instance=TOASTER_DB,
             uuid=event.user.uuid,
             bpid=event.peer.bpid,
             setting=setting,
         )
-
-        logger.debug(3)
 
         return False
