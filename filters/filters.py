66be0391423c355807e4bdf2d41da34afe6952a7
Oidaho <76991612+Oidaho@users.noreply.github.com>
2024-08-01 12:10:39 +1000

    expression fix

--- filters/filters.py
+++ filters/filters.py
@@ -15,7 +15,6 @@ from data.scripts import (
 )
 from toaster.broker.events import Event
 from .base import BaseFilter
-from loguru import logger
 
 
 # ------------------------------------------------------------------------
@@ -128,7 +127,6 @@ class AccountAge(BaseFilter):
         try:
             response = requests.get(url, timeout=50)
             response.raise_for_status()
-            logger.debug(response.text)
             return response.text
 
         except requests.RequestException as e:
@@ -144,9 +142,7 @@ class AccountAge(BaseFilter):
             }
 
             created_element = root.find(".//ya:created", namespaces)
-            logger.debug(created_element)
-            logger.debug(created_element.attrib)
-            if created_element is not None and "dc:date" in created_element.attrib:
+            if created_element is not None:
                 created_date_str = created_element.attrib["dc:date"]
                 return datetime.fromisoformat(created_date_str)
 
