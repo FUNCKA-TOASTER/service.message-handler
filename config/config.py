b48d499e063f783c2871838a6a7ca59878b44b45
Oidaho <76991612+Oidaho@users.noreply.github.com>
2024-08-26 14:26:44 +1000

    Some changes

--- config/config.py
+++ config/config.py
@@ -11,7 +11,6 @@ About:
 import os
 from funcka_bots.credentials import RabbitMQCredentials
 
-# Redis (broker) credentials
 BROKER_CREDS = RabbitMQCredentials(
     host=os.getenv("rabbitmq_host"),
     port=os.getenv("rabbitmq_port"),
@@ -20,14 +19,10 @@ BROKER_CREDS = RabbitMQCredentials(
     pswd=os.getenv("rabbitmq_pswd"),
 )
 
-# Broker subscription channel name
 BROKER_QUEUE_NAME = "message"
 
-# API token obtained from environment variable
 VK_GROUP_TOKEN: str = os.getenv("vk_group_token")
 
-# Group ID for identifying specific groups
 VK_GROUP_ID: int = int(os.getenv("vk_group_id"))
 
-# API version used for API requests
 VK_API_VERSION: str = "5.199"
